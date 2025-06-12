3968da1f31c7ea69733c25c038119e0a210bca4a
developerUmair <umairmak143@gmail.com>
2025-06-13 00:30:48 +0500

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,54 +1,31 @@
 import React from "react";
-import { Box, Container, Typography, Link } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <Box
       component="footer"
       sx={{
         background: "linear-gradient(90deg, #0f2027, #203a43, #2c5364)",
         boxShadow: "0 4px 20px rgba(0,0,0,0.4)",
-        p: 2,
+        py: 4,
+        px: 2,
         color: "white",
+        textAlign: "center",
       }}
     >
-      <Container
-        maxWidth="lg"
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          flexWrap: "wrap",
-        }}
-      >
-        <Box>
-          <Typography variant="h6">🎬 MovieMania</Typography>
-          <Typography variant="body2" color="gray">
-            Your daily dose of movies
-          </Typography>
-        </Box>
-        <Box sx={{ display: "flex", gap: 3 }}>
-          {["Home", "About", "Contact"].map((text) => (
-            <Link
-              key={text}
-              href="#"
-              underline="none"
-              sx={{
-                color: "gray",
-                fontWeight: 500,
-                letterSpacing: 0.5,
-                transition: "all 0.3s ease",
-                "&:hover": {
-                  color: "white",
-                  transform: "scale(1.05)",
-                  textDecoration: "underline",
-                },
-              }}
-            >
-              {text}
-            </Link>
-          ))}
-        </Box>
+      <Container maxWidth="lg">
+        <Typography variant="h6" sx={{ fontWeight: "bold"}}>
+          🎬 MovieMania
+        </Typography>
+        <Typography variant="body2" color="gray" mb={2}>
+          Your daily dose of movies
+        </Typography>
+        <Typography variant="body2" color="gray">
+          All rights reserved © {currentYear}
+        </Typography>
       </Container>
     </Box>
   );
